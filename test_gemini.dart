1054d54e0507f4060e3a7c3a39503bee6257e9bb
Sneh Prasad <snehprasad07@gmail.com>
2026-08-29 23:39:25 +0530

    chore: environment variable retrieval

--- test_gemini.dart
+++ test_gemini.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final apiKey = 'AQ.Ab8RN6JvvYhPqZ3EYWQaZP9ZJ-iwLI3XCA5FmOmNajDlhLAUBQ';
+  final apiKey = const String.fromEnvironment('GEMINI_API_KEY', defaultValue: 'YOUR_API_KEY_HERE');
   
   // Test 1: List Models
   final response1 = await http.get(
